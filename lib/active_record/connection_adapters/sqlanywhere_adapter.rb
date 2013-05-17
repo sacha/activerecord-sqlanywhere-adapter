1440eac5459c03307e4d3e8a1b7ac8f2073c6a84
Sacha Bernstein <sacha@mdabilling.com>
2013-05-17 10:56:38 -0400

    Add the option of passing an array as a column or table name, which will map as: ['a','b'] -> "a"."b"

--- lib/active_record/connection_adapters/sqlanywhere_adapter.rb
+++ lib/active_record/connection_adapters/sqlanywhere_adapter.rb
@@ -230,9 +230,15 @@ module ActiveRecord
 
       # Applies quotations around column names in generated queries
       def quote_column_name(name) #:nodoc:
-        # Remove backslashes and double quotes from column names
-        name = name.to_s.gsub(/\\|"/, '')
-        %Q("#{name}")
+        if name.is_a? Array
+          # Remove backslashes and double quotes from column names
+          name = name.map { |n| n.to_s.gsub(/\\|"/, '') }
+          %Q("#{name.join('","')}")
+        else
+          # Remove backslashes and double quotes from column names
+          name = name.to_s.gsub(/\\|"/, '')
+          %Q("#{name}")
+        end
       end
 
       # Handles special quoting of binary columns. Binary columns will be treated as strings inside of ActiveRecord.
